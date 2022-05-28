aea2a835cf0bffdf66e3a368620c619fbd1fdea2
# <dmagats123@gmail.com>
2022-05-27 19:29:36 -0700

    Remove engine import in tasks

--- internal/tasks/tasks.go
+++ internal/tasks/tasks.go
@@ -2,7 +2,6 @@ package tasks
 
 import (
 	"github.com/Nano-Software/Pace/configs"
-	"github.com/Nano-Software/Pace/internal/engine"
 )
 
 func InitializeTasks(config *configs.Config) error {
@@ -14,13 +13,6 @@ func InitializeTasks(config *configs.Config) error {
 		go func() {
 			// TODO: Setup context and cancel function here possibly create instance of task but that may result in unnecessary allocation of memory however we could reassign it as nil
 
-			engine.StartTask(&Task{
-				TaskIsActive: false,
-				Module:       config.Module,
-				Input:        config.Input,
-				Secret:       config.Secret,
-				Monitor:      make(chan interface{}),
-			})
 		}()
 	}
 
